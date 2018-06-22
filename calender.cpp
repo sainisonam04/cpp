b5c1ff9619c99ce103a185dcb43fa6822d9230f7
Sameer Saini <38816886+Ghost007saini@users.noreply.github.com>
2018-06-22 15:23:57 +0530

    calender
    learning cpp

---
+++ calender.cpp
@@ -0,0 +1,27 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{int a,b,c;
+cout<<"welcome to calender";
+cout<<"enter week day:";
+cin>>a;
+switch (c==a)
+{case 1: cout<<"monday \n";
+         break;
+ case 2:cout<<"\n teusday";
+        break;
+ case 3: cout<<"\n wednesday";
+ break;
+ case 4: cout<<"\n thursday";
+ break;
+ case 5:cout<<"\n friday";
+ break;
+ case 6:cout<<"\n saturday";
+ break;
+ case 7:cout<<"\n sunday";
+ break;
+ default:cout<<"\n worng no.";
+ }
+ getch ();
+ return 0;
+ }
